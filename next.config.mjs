164ee727d867d6cb38c09b30d8dbbd10324d236c
Scott Motte <mot@mot.la>
2023-07-17 14:06:27 -0700

    Make npm run build work

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,10 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  images: {
+    unoptimized: true, // https://stackoverflow.com/questions/75785972/nextjs-export-folder-out-doesnt-have-index-html
+  },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   experimental: {
