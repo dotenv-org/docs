ce1f7accaaa3ea27479b955745c73aad605d5057
Scott Motte <mot@mot.la>
2023-07-31 15:59:53 -0700

    Add sinatra example

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -108,8 +108,8 @@ export function Frameworks() {
             <h3 className="text-sm font-semibold text-zinc-900 dark:text-white">
               Sinatra
             </h3>
-            <p className="mt-1 text-sm text-zinc-600 dark:text-zinc-400">Use .env.vault with Sinatra</p>
-            <p className="mt-4"><Button href="/docs/languages/nodejs/sinatra" variant="text" arrow="right">Start</Button></p>
+            <p className="mt-1 text-sm text-zinc-600 dark:text-zinc-400">Deploy a Sinatra App to Heroku</p>
+            <p className="mt-4"><Button href="/docs/frameworks/sinatra/heroku" variant="text" arrow="right">Read guide</Button></p>
           </div>
           <SinatraIcon className="h-9 w-9 text-[#000000] dark:invert"/>
         </div>
