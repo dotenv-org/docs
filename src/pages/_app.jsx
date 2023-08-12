f01bcd7cf9f9cd912504e077aec2f9cc8f4d96a2
Scott Motte <mot@mot.la>
2023-08-12 08:40:08 -0700

    Add og-url

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -19,7 +19,9 @@ Router.events.on('hashChangeStart', onRouteChange)
 
 export default function App({ Component, pageProps }) {
   let router = useRouter()
-
+  let url = `https://www.dotenv.org${router.asPath}`
+  let encodedUrl = encodeURIComponent(url)
+  
   return (
     <>
       <Head>
@@ -29,6 +31,17 @@ export default function App({ Component, pageProps }) {
           <title>{`${pageProps.title} | Dotenv`}</title>
         )}
         <meta name="description" content={pageProps.description} />
+        <meta property="og:title" content={pageProps.title} />
+        <meta property="og:description" content={pageProps.description} />
+        <meta property="og:url" content={url} />
+        {process.env.NODE_ENV === 'production' ? (
+          <>
+            <meta property="og:image" content={`https://api.urlbox.io/v1/Ep4Ip4nNnFMNUg9J/png?url=${encodedUrl}&width=800&height=600&fail_on_4xx=true&fail_on_5xx=true`} />
+          </>
+        ) : (
+          <>
+          </>
+        )}
 
         <Analytics />
       </Head>
