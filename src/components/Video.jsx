60d1f02f8236631b43a245b8217d60eacc0d589f
Scott Motte <mot@mot.la>
2023-11-05 06:06:51 -0800

    Add compare environments short video tutorial

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,6 +1,6 @@
 export function Video(props) {
   return (
-    <div className="rounded-md p-7 border border-zinc-200 dark:border-zinc-700 bg-zinc-100 dark:bg-zinc-100">
+    <div className="rounded-md border border-zinc-200 dark:border-zinc-700">
       <video controls poster={props.poster} className="w-full aspect-video rounded-md m-0 shadow-lg">
         <source src={props.mp4} type="video/mp4" />
         <source src={props.webm} type="video/webm" />
